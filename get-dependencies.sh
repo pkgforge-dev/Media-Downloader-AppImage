46730c26e8f5f220f388ccd1b7d288fa18abc915
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-05 18:40:11 -0400

    use `bun` instead of `deno`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm aria2 deno yt-dlp
+pacman -Syu --noconfirm aria2 yt-dlp bun
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -14,6 +14,13 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 make-aur-package media-downloader
+
+# yt-dlp-ejs archlinux package has a hard dependency on deno
+# but this can actually use bun instead
+pacman -Rdd --noconfirm deno || :
+
+# yt-dlp also gives a warning that only deno is supported by default
+sed -i -e "s|default=\['deno'\]|default=['bun']|" /usr/lib/python*/site-packages/yt_dlp/options.py 
 
 # If the application needs to be manually built that has to be done down here
 
