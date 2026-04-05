a26a4a3773c0e2ffd5f415594f38a4542a600fba
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-05 06:50:50 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,23 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q media-downloader | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/pixmaps/media-downloader.png
+export DESKTOP=/usr/share/applications/media-downloader.desktop
+export DEPLOY_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/media-downloader \
+	/usr/bin/wget             \
+	/usr/bin/aria2c           \
+	/usr/bin/yt-dlp           \
+	/usr/bin/deno             \
+	/usr/bin/ffmpeg
 
 # Additional changes can be done in between here
 
