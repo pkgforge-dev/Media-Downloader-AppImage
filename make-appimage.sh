8a62b7a8cb76e3531e2d4cd50fe10d298fe4c380
Samueru-sama <xdglawyer@outlook.com>
2026-09-04 01:10:51 -0400

    use host drivers

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/pixmaps/media-downloader.png
 export DESKTOP=/usr/share/applications/media-downloader.desktop
 export DEPLOY_PYTHON=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun \
